df6c80a727d24e885b1a7498c3ddaa920b37977d
Damicode <damierraymond@yahoo.fr>
2024-02-20 19:32:08 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,9 +3,6 @@ provider "aws" {
   region = "us-east-1"  # Replace with your desired AWS region.
 }
 
-variable "cidr" {
-  default = "10.0.0.0/16"
-}
 
 resource "aws_key_pair" "example" {
   key_name   = "terraform-demo-abhi"  # Replace with your desired key name
@@ -72,34 +69,77 @@ resource "aws_security_group" "webSg" {
   }
 }
 
+resource "aws_s3_bucket" "s3_bucket" {
+  bucket = var.S3 # change this name at variable.tf file
+}
+
 resource "aws_instance" "server" {
-  ami                    = "ami-0261755bbcb8c4a84"
-  instance_type          = "t2.micro"
-  key_name      = aws_key_pair.example.key_name
+  ami                    = var.images
+  instance_type          = lookup(var.Instance_type,terraform.workspace,"t2.micro")
+  #key_name      = aws_key_pair.example.key_name
   vpc_security_group_ids = [aws_security_group.webSg.id]
   subnet_id              = aws_subnet.sub1.id
 
-  connection {
-    type        = "ssh"
-    user        = "ubuntu"  # Replace with the appropriate username for your EC2 instance
-    private_key = file("~/.ssh/id_rsa")  # Replace with the path to your private key
-    host        = self.public_ip
-  }
+  # connection {
+  #   type        = "ssh"
+  #   user        = "ubuntu"  # Replace with the appropriate username for your EC2 instance
+  #   private_key = file("~/.ssh/id_rsa")  # Replace with the path to your private key
+  #   host        = self.public_ip
+  # }
 
   # File provisioner to copy a file from local to the remote EC2 instance
-  provisioner "file" {
-    source      = "app.py"  # Replace with the path to your local file
-    destination = "/home/ubuntu/app.py"  # Replace with the path on the remote instance
-  }
+  # provisioner "file" {
+  #   source      = "app.py"  # Replace with the path to your local file
+  #   destination = "/home/ubuntu/app.py"  # Replace with the path on the remote instance
+  # }
 
-  provisioner "remote-exec" {
-    inline = [
-      "echo 'Hello from the remote instance'",
-      "sudo apt update -y",  # Update package lists (for ubuntu)
-      "sudo apt-get install -y python3-pip",  # Example package installation
-      "cd /home/ubuntu",
-      "sudo pip3 install flask",
-      "sudo python3 app.py &",
-    ]
+  # provisioner "remote-exec" {
+  #   inline = [
+  #     "echo 'Hello from the remote instance'",
+  #     "sudo apt update -y",  # Update package lists (for ubuntu)
+  #     "sudo apt-get install -y python3-pip",  # Example package installation
+  #     "cd /home/ubuntu",
+  #     "sudo pip3 install flask",
+  #     "sudo python3 app.py &",
+  #   ]
+  # }
+
+  tags = {
+    Name = var.EKS
+  }
+}
+
+resource "aws_instance" "server2" {
+  ami                    = var.images
+  instance_type          = lookup(var.Instance_type,terraform.workspace,"t2.micro")
+  #key_name      = aws_key_pair.example.key_name
+  vpc_security_group_ids = [aws_security_group.webSg.id]
+  subnet_id              = aws_subnet.sub1.id
+
+  tags = {
+    Name = var.Jenkins
+  }
+}
+
+resource "aws_instance" "server3" {
+  ami                    = var.images
+  instance_type          = lookup(var.Instance_type,terraform.workspace,"t2.micro")
+  #key_name      = aws_key_pair.example.key_name
+  vpc_security_group_ids = [aws_security_group.webSg.id]
+  subnet_id              = aws_subnet.sub1.id
+
+  tags = {
+    Name = var.Ansible
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_lock" {
+  name           = var.terraform
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
   }
 }
